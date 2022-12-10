cda2aa8db5c299e70012d10529519796f3c4caa3
Justin Dela Cruz <jdelacruz@nerdwallet.com>
2022-12-10 14:00:16 -0800

    Day 03 - Part 1

--- src/day-03-rucksack-organization/day-03.ts
+++ src/day-03-rucksack-organization/day-03.ts
@@ -1,8 +1,50 @@
-import { readTextFile } from "../utils";
+import { print, readTextFile } from "../utils";
 
 const inputFilename = "./inputs/day-03.txt";
 
-const partOne = (input: string[]) => {};
+const getPriority = (item: string) => {
+  const priority = item.charCodeAt(0);
+
+  // A-Z => ASCII 65-90
+  // a-z => ASCII 97-122
+  return priority < 97 ? priority - 64 + 26: priority - 96;
+};
+
+const getDuplicatedItem = (
+  aCompartment: string,
+  bCompartment: string
+): string => {
+  const aSet = new Set(aCompartment);
+  const bSet = new Set(bCompartment);
+
+  let duplicatedItem = "";
+  aSet.forEach((item) => {
+    if (bSet.has(item)) {
+      duplicatedItem = item;
+    }
+  });
+
+  return duplicatedItem;
+};
+
+const partOne = (input: string[]) => {
+  let totalPriority = 0;
+
+  input.forEach((rucksack) => {
+    const numItems = rucksack.length;
+    const aCompartment = rucksack.slice(0, numItems / 2);
+    const bCompartment = rucksack.slice(numItems / 2);
+
+    const duplicatedItem = getDuplicatedItem(aCompartment, bCompartment);
+    const priority = getPriority(duplicatedItem);
+
+    print(duplicatedItem, priority);
+
+    totalPriority += priority;
+  });
+
+  print(`The sum of the priorities is ${totalPriority}`);
+};
 
 export default function () {
   const input = readTextFile(inputFilename);
