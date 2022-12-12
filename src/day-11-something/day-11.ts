89d66b8951cb25e4a0d674db05c15fced803be8e
Justin Dela Cruz <jdelacruz@nerdwallet.com>
2022-12-11 16:32:04 -0800

    Day 11 - Part 2

--- src/day-11-something/day-11.ts
+++ src/day-11-something/day-11.ts
@@ -1,44 +1,35 @@
 // https://adventofcode.com/2022/day/11
-import Big from "big.js";
 import { strict as assert } from "node:assert";
 import { print, readTextFile } from "../utils";
-import { inspect } from "util";
+import { Item, Monkey, Operand, Operation, Operator } from "./types";
 
-Big.RM = Big.roundDown;
+const inputFilename = "./inputs/day-11.txt";
 
-const inputFilename = "./inputs/day-11-test.txt";
-
-const getMonkeyId = (input: string): number => {
-  // Monkey 0
+const parseMonkeyId = (input: string): number => {
   const [, monkeyIdStr] = /Monkey (\d*?):/.exec(input) ?? [];
 
   return parseInt(monkeyIdStr, 10);
 };
 
-const getStartingItems = (input: string): Big[] => {
-  // Starting items: 54, 65, 75, 74
+const parseStartingItems = (input: string): number[] => {
   const [, startingItemsStr] = /Starting items: (.*?)$/.exec(input) ?? [];
-  const startingItemsStrArray = startingItemsStr.split(", ");
 
-  return startingItemsStrArray.map((startingItemStr) => new Big(parseInt(startingItemStr, 10)));
+  return startingItemsStr.split(", ").map((startingItemStr) => parseInt(startingItemStr, 10));
 };
 
-const Operator = {
-  Add: "+",
-  Subtract: "-",
-  Multiply: "*",
-} as const;
-type OperatorKey = keyof typeof Operator;
-type Operator = typeof Operator[OperatorKey];
+const parseItems = (input: string): Item[] => {
+  const [, startingItemsStr] = /Starting items: (.*?)$/.exec(input) ?? [];
+  const startingItems = startingItemsStr
+    .split(", ")
+    .map((startingItemStr) => parseInt(startingItemStr, 10));
 
-type Operand = number | "old";
-
-type Operation = {
-  operator: Operator;
-  operand: Operand;
+  return startingItems.map((startingItem) => ({
+    startingValue: startingItem,
+    remainders: {},
+  }));
 };
 
-const getOperation = (input: string): Operation => {
+const parseOperation = (input: string): Operation => {
   const [, operator, operandStr] = /Operation: new = old (.) (.*)$/.exec(input) ?? [];
 
   const operand = operandStr !== "old" ? parseInt(operandStr, 10) : operandStr;
@@ -49,48 +40,40 @@ const getOperation = (input: string): Operation => {
   };
 };
 
-const getDivisibilityTest = (input: string): number => {
+const parseDivisibiltyTest = (input: string): number => {
   const [, divisor] = /Test: divisible by (.*)$/.exec(input) ?? [];
 
   return parseInt(divisor, 10);
 };
 
-const getTargetMonkeyIfTrue = (input: string): number => {
+const parseTargetMonkeyIfTrue = (input: string): number => {
   const [, targetMonkey] = /If true: throw to monkey (.*)$/.exec(input) ?? [];
 
   return parseInt(targetMonkey, 10);
 };
 
-const getTargetMonkeyIfFalse = (input: string): number => {
+const parseTargetMonkeyIfFalse = (input: string): number => {
   const [, targetMonkey] = /If false: throw to monkey (.*)$/.exec(input) ?? [];
 
   return parseInt(targetMonkey, 10);
-};
-
-type Monkey = {
-  id: number;
-  startingItems: Big[];
-  operation: Operation;
-  divisibilityTest: number;
-  targetMonkeyIfTrue: number;
-  targetMonkeyIfFalse: number;
-  inspectionCount: number;
 };
 
 const parseInputs = (inputs: string[]): Monkey[] => {
   const monkeys: Monkey[] = [];
 
   for (let i = 0; i < inputs.length; i++) {
-    const monkeyId = getMonkeyId(inputs[i]);
-    const startingItems = getStartingItems(inputs[i + 1]);
-    const operation = getOperation(inputs[i + 2]);
-    const divisibilityTest = getDivisibilityTest(inputs[i + 3]);
-    const targetMonkeyIfTrue = getTargetMonkeyIfTrue(inputs[i + 4]);
-    const targetMonkeyIfFalse = getTargetMonkeyIfFalse(inputs[i + 5]);
+    const monkeyId = parseMonkeyId(inputs[i]);
+    const startingItems = parseStartingItems(inputs[i + 1]);
+    const items = parseItems(inputs[i + 1]);
+    const operation = parseOperation(inputs[i + 2]);
+    const divisibilityTest = parseDivisibiltyTest(inputs[i + 3]);
+    const targetMonkeyIfTrue = parseTargetMonkeyIfTrue(inputs[i + 4]);
+    const targetMonkeyIfFalse = parseTargetMonkeyIfFalse(inputs[i + 5]);
 
     monkeys.push({
       id: monkeyId,
       startingItems,
+      items,
       operation,
       divisibilityTest,
       targetMonkeyIfTrue,
@@ -101,62 +84,108 @@ const parseInputs = (inputs: string[]): Monkey[] => {
     i = i + 6;
   }
 
+  // Pre-calculate the remainders
+  const divisibilityTests = monkeys.map((monkey) => monkey.divisibilityTest);
+  monkeys.forEach((monkey) => {
+    monkey.items.forEach((item) => {
+      divisibilityTests.forEach((divisibilityTest) => {
+        item.remainders[divisibilityTest] = item.startingValue % divisibilityTest;
+      });
+    });
+  });
+
   return monkeys;
 };
 
-const play = (monkeys: Monkey[], shouldDivideByThree: boolean, shouldPrint: boolean) => {
-  monkeys.forEach((monkey, index) => {
-    shouldPrint && print();
-    shouldPrint && print(`Monkey ${monkey.id}:`);
+const play = (monkeys: Monkey[], shouldPrint: boolean, useAlternateGameplay: boolean) => {
+  monkeys.forEach((monkey) => {
+    if (shouldPrint) {
+      print();
+      print(`Monkey ${monkey.id}:`);
+    }
 
-    monkey.startingItems.forEach((worryLevel) => {
-      monkey.inspectionCount += 1;
+    if (useAlternateGameplay) {
+      // Instead of calculating worry levels directly,
+      // we'll work indirectly using the modulus remainders.
+      monkey.items.forEach((item) => {
+        monkey.inspectionCount += 1;
 
-      shouldPrint && print(`  Monkey inspects item with a worry level of ${worryLevel}.`);
-      const operand = monkey.operation.operand === "old" ? worryLevel : monkey.operation.operand;
+        for (const [divisorStr, remainder] of Object.entries(item.remainders)) {
+          const operand =
+            monkey.operation.operand === "old"
+              ? item.remainders[divisorStr]
+              : monkey.operation.operand;
 
-      let newWorryLevel = worryLevel;
-      if (monkey.operation.operator === Operator.Add) {
-        newWorryLevel = worryLevel.plus(operand);
-        shouldPrint && print(`    Worry level is added by ${operand} to ${newWorryLevel}`);
-      }
-      if (monkey.operation.operator === Operator.Subtract) {
-        newWorryLevel = worryLevel.minus(operand);
-        shouldPrint && print(`    Worry level is subtracted by ${operand} to ${newWorryLevel}`);
-      }
-      if (monkey.operation.operator === Operator.Multiply) {
-        newWorryLevel = worryLevel.times(operand);
-        shouldPrint && print(`    Worry level is multiplied by ${operand} to ${newWorryLevel}`);
-      }
+          if (monkey.operation.operator === Operator.Add) {
+            item.remainders[divisorStr] = (remainder + operand) % parseInt(divisorStr, 10);
+          }
+          if (monkey.operation.operator === Operator.Subtract) {
+            item.remainders[divisorStr] = (remainder - operand) % parseInt(divisorStr, 10);
+          }
+          if (monkey.operation.operator === Operator.Multiply) {
+            item.remainders[divisorStr] = (remainder * operand) % parseInt(divisorStr, 10);
+          }
+        }
 
-      if (shouldDivideByThree) {
-        newWorryLevel = newWorryLevel.div(3).round(Big.roundDown);
+        if (item.remainders[monkey.divisibilityTest] === 0) {
+          monkeys[monkey.targetMonkeyIfTrue].items.push(item);
+        } else {
+          monkeys[monkey.targetMonkeyIfFalse].items.push(item);
+        }
+      });
+
+      monkey.items = [];
+    } else {
+      // Strategy: Calculate worry level directly
+      monkey.startingItems.forEach((worryLevel) => {
+        monkey.inspectionCount += 1;
+
+        if (shouldPrint) print(`  Monkey inspects item with a worry level of ${worryLevel}.`);
+
+        const operand = monkey.operation.operand === "old" ? worryLevel : monkey.operation.operand;
+        let newWorryLevel = worryLevel;
+
+        if (monkey.operation.operator === Operator.Add) {
+          newWorryLevel = worryLevel + operand;
+          if (shouldPrint) print(`    Worry level is added by ${operand} to ${newWorryLevel}`);
+        }
+        if (monkey.operation.operator === Operator.Subtract) {
+          newWorryLevel = worryLevel - operand;
+          if (shouldPrint) print(`    Worry level is subtracted by ${operand} to ${newWorryLevel}`);
+        }
+        if (monkey.operation.operator === Operator.Multiply) {
+          newWorryLevel = worryLevel * operand;
+          if (shouldPrint) print(`    Worry level is multiplied by ${operand} to ${newWorryLevel}`);
+        }
+
+        newWorryLevel = Math.floor(newWorryLevel / 3);
         shouldPrint &&
           print(`    Monkey gets bored with item. Worry level is divided by 3 to ${newWorryLevel}`);
-      }
 
-      const divisibilityTest = newWorryLevel.mod(monkey.divisibilityTest);
-      if (divisibilityTest.eq(0)) {
-        shouldPrint && print(`    Current worry level is divisible by ${monkey.divisibilityTest}.`);
-        shouldPrint &&
-          print(
-            `    Item with worry level ${newWorryLevel} is thrown to Monkey ${monkey.targetMonkeyIfTrue}`
-          );
+        const divisibilityTest = newWorryLevel % monkey.divisibilityTest;
+        if (divisibilityTest === 0) {
+          if (shouldPrint) {
+            print(`    Current worry level is divisible by ${monkey.divisibilityTest}.`);
+            print(
+              `    Item with worry level ${newWorryLevel} is thrown to Monkey ${monkey.targetMonkeyIfTrue}`
+            );
+          }
 
-        monkeys[monkey.targetMonkeyIfTrue].startingItems.push(newWorryLevel);
-      } else {
-        shouldPrint &&
-          print(`    Current worry level is not divisible by ${monkey.divisibilityTest}.`);
-        shouldPrint &&
-          print(
-            `    Item with worry level ${newWorryLevel} is thrown to Monkey ${monkey.targetMonkeyIfFalse}`
-          );
+          monkeys[monkey.targetMonkeyIfTrue].startingItems.push(newWorryLevel);
+        } else {
+          if (shouldPrint) {
+            print(`    Current worry level is not divisible by ${monkey.divisibilityTest}.`);
+            print(
+              `    Item with worry level ${newWorryLevel} is thrown to Monkey ${monkey.targetMonkeyIfFalse}`
+            );
+          }
 
-        monkeys[monkey.targetMonkeyIfFalse].startingItems.push(newWorryLevel);
-      }
-    });
+          monkeys[monkey.targetMonkeyIfFalse].startingItems.push(newWorryLevel);
+        }
+      });
 
-    monkey.startingItems = [];
+      monkey.startingItems = [];
+    }
   });
 };
 
@@ -180,31 +209,30 @@ const calculateMonkeyBusiness = (monkeys: Monkey[]): number => {
 };
 
 const partOne = (inputs: string[]) => {
+  const numRounds = 20;
   const monkeys = parseInputs(inputs);
-  for (let i = 0; i < 20; i++) {
-    play(monkeys, true, true);
-  }
-
-  const monkeyBusiness = calculateMonkeyBusiness(monkeys);
-  print(`[Part 1] After 20 rounds of play, the level of monkey business is ${monkeyBusiness}.`);
-  // assert(monkeyBusiness === 88208);
-  assert(monkeyBusiness === 10605);
-};
-
-const partTwo = (inputs: string[]) => {
-  const numGames = 20;
-  const monkeys = parseInputs(inputs);
-  for (let i = 0; i < numGames; i++) {
-    print(`Game ${i}`);
-    play(monkeys, false, false);
+  for (let i = 0; i < numRounds; i++) {
+    play(monkeys, true, false);
   }
 
   const monkeyBusiness = calculateMonkeyBusiness(monkeys);
   print(
-    `[Part 2] After ${numGames} rounds of play, the level of monkey business is ${monkeyBusiness}.`
+    `[Part 1] After ${numRounds} rounds of play, the level of monkey business is ${monkeyBusiness}.`
   );
+  assert(monkeyBusiness === 88208); // real file
+};
 
-  assert(monkeyBusiness === 10197);
+const partTwo = (inputs: string[]) => {
+  const numRounds = 10000;
+  const monkeys = parseInputs(inputs);
+  for (let i = 0; i < numRounds; i++) {
+    play(monkeys, false, true);
+  }
+
+  const monkeyBusiness = calculateMonkeyBusiness(monkeys);
+  print(
+    `[Part 2] After ${numRounds} rounds of play, the level of monkey business is ${monkeyBusiness}.`
+  );
 };
 
 export default function () {
